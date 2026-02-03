b08ae4e774307c2634899274abcce0b06504dca8
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-02-03 22:41:50 +0530

    delay.js solved

--- callbacks/easy/delay.js
+++ callbacks/easy/delay.js
@@ -3,6 +3,13 @@
 // You are required to write a function named delay that takes two parameters: a time duration in milliseconds and a value. 
 // The function should return a Promise that waits for the given time and then resolves with the provided value.
 
-function delay(ms, value, callback) {}
+function delay(ms, value,callback) {
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            callback(null,value);
+            resolve(value);
+        },ms)
+    })
+}
 
 module.exports = delay;
