7fe68401d56e8e7dcc8ec8958b68fda3a5adc07a
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-02-04 14:24:02 +0530

    fetchWithTimout & once solved

--- callbacks/easy/fetchWithTimeout.js
+++ callbacks/easy/fetchWithTimeout.js
@@ -5,6 +5,32 @@
 // If the request completes within the specified time, the Promise resolves with the fetched data. 
 // If the operation exceeds the time limit, the Promise rejects with the message "Request Timed Out".
 
-function fetchWithTimeout(url, ms, callback) {}
+function fetchWithTimeout(url, ms, callback) {
+    return new Promise((resolve, reject) => {
+        let lockState = false;
+
+        setTimeout(() => {
+            if (lockState) return
+            lockState = true;
+            const err = "Request Timed Out"
+            callback(err, null)
+            resolve(err) 
+        }, ms)
+
+        fetch(url, (err, data) => {
+            if (lockState) return
+            lockState = true
+            
+            if (err) {
+                callback(err, null)
+                resolve(err)
+            }
+            else {
+                callback(null, data)
+                resolve(data)
+            }
+        })
+    })
+}
 
 module.exports = fetchWithTimeout;
--- callbacks/easy/once.js
+++ callbacks/easy/once.js
@@ -3,6 +3,64 @@
 // You are required to implement a wrapper function named once that accepts an asynchronous function fn.
 // The wrapper should ensure that fn is executed only on the first call.
 // Any subsequent calls must not re-execute fn and should instead return the same Promise or resolved result from the first invocation.
-function once(fn) {}
+// function once(fn) {
+//     let promise;
+//     return function (...args) {
+//         if (promise) return {promise,callback}
 
+//         promise = new Promise((resolve, reject) => {
+//             const callback = args.pop();
+//             fn(...args,(err,data)=>{
+
+//                 if (data) {
+//                     callback(null,data)
+//                     resolve(data)
+//                 }
+//                 else {
+//                     callback(err,null)
+//                     reject(err)
+//                 }
+//             })
+            
+
+//         })
+//         return promise
+//     }
+// }
+
+
+function once(fn){
+    let promise ;
+    return function(...args){
+        
+        let cb = args.pop()
+
+        if(promise){
+            promise
+            .then(
+                data => cb(null,data)
+            )
+            .catch(
+                err => cb(err,null)
+            )
+            return
+        }
+
+        promise = new Promise((resolve,reject)=> {
+            fn(...args,(err,data)=>{
+                if(err){
+                    reject(err)
+                }
+                else{
+                    resolve(data)
+                }
+            }
+        )
+    }
+    )
+    promise
+    .then(data=> cb(null,data))
+    .catch(err=>cb(err,null))
+    }
+}
 module.exports = once;
