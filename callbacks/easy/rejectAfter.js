c3a13a084a08f0717fff5e408a951f6812a41123
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-02-04 15:33:20 +0530

    rejectAfter solved

--- callbacks/easy/rejectAfter.js
+++ callbacks/easy/rejectAfter.js
@@ -3,7 +3,20 @@
 // You are required to create a function named rejectAfter that accepts a time duration in milliseconds. 
 // The function should return a Promise that waits for the specified time and then rejects.
 
-function rejectAfter(ms, callback) {}
+function rejectAfter(ms, callback) {
+    const promise =  new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const err = new Error(`Rejected after ${ms}ms`)
+            callback(err,null)
+            reject(err)
+        },ms)
+    });
+    
+    if(callback) promise.catch(()=>{})
+        
+return promise
+    
+}
 
 module.exports = rejectAfter;
 
