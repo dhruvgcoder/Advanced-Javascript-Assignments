b84cf1253b4ef10776e7821b7354b0dfe0119942
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-02-04 15:53:45 +0530

    retryOnce Solved

--- callbacks/easy/retryOnce.js
+++ callbacks/easy/retryOnce.js
@@ -1,8 +1,28 @@
 // Problem Description – Retry Async Function Once
 
-// You are given an asynchronous function fn. Your task is to return a new function that calls fn and retries it once if the first attempt fails. 
+// You are given an asynchronous function fn. 
+// Your task is to return a new function that calls fn and retries it once if the first attempt fails. 
 // If the second attempt also fails, the error should be properly propagated. 
 
-function retryOnce(fn) {}
+function retryOnce(fn) {
+    let attempted = false;
+    return function (...args) {
+        const cb = args.pop()
+
+        function tries() {
+            fn(...args, (err, data) => {
+
+                if (!err) return cb(null, data)
+                if (attempted) return cb(err, null)
+                attempted = true
+                tries()
+
+            })
+        }
+        tries()
+
+    }
+
+}
 
 module.exports = retryOnce;
